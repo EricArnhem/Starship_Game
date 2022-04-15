697ed6a1abbfef576582c3b385de17f7a741a20e
Eric Arnhem <arnhemeric60@gmail.com>
2022-04-15 21:40:04 +0200

    Change navbar style Includes colored SVG icons

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,13 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav>
       <ul id="navigation-links">
-        <li><RouterLink to="/"><img src="/svg/dashboard.svg" title="Dashboard" class="filter-white"></RouterLink></li>
-        <li><RouterLink to="/manage-starships"><img src="/svg/rocket.svg" title="Manage Starships" class="filter-white"></RouterLink></li>
-      </ul>      
+        <li>
+          <RouterLink to="/"><img src="/svg/dashboard.svg" title="Dashboard"></RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/manage-starships"><img src="/svg/rocket.svg" title="Manage Starships"></RouterLink>
+        </li>
+      </ul>
     </nav>
   </header>
 
@@ -18,12 +22,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style>
-
-/* Filter used to change the svgs color to white (#f5f5f5) */
-.filter-white {
-  filter: invert(92%) sepia(65%) saturate(5592%) hue-rotate(186deg) brightness(129%) contrast(92%);
-}
-
 body {
   background-color: #181818;
   font-family: sans-serif;
@@ -40,33 +38,23 @@ nav {
   min-height: 100vh;
   width: 45px;
   border-right: 1px solid rgba(146, 146, 146, 0.75);
+  box-shadow: 1px 0 1px rgba(146, 146, 146, 0.75);
   padding: 0 0.3rem;
   /* temporary margin */
-  margin-right: 1rem; 
-}
-
-a {
-  text-decoration: none;
-  color: hsl(216, 85%, 52%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(216, 85%, 52%, 0.2);
-  }
-}
-
-nav a.router-link-exact-active {
-  color: rgba(235, 235, 235, 0.64);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  margin-right: 1rem;
 }
 
 nav a {
   display: inline-block;
+}
+
+nav a.router-link-exact-active {
+  filter: drop-shadow(3px 3px 2px #dddddd);
+}
+
+nav a:hover {
+  filter: drop-shadow(3px 3px 2px #dddddd);
+  transition: 0.1s;
 }
 
 #navigation-links {
@@ -85,7 +73,7 @@ nav a {
   border-bottom: none;
 }
 
-#navigation-links li img{
+#navigation-links li img {
   display: block;
   height: 45px;
 }
