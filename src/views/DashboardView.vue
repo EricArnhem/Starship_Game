62a6c47c3ef522f2000766404c468b036312b482
Eric Arnhem <arnhemeric60@gmail.com>
2022-04-27 17:29:04 +0200

    Add starship name to starship info

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -7,7 +7,10 @@ import StatsTable from '../components/StatsTable.vue';
 <template>
   <AlertScreen />
   <div id="starship-data">
-    <StatsTable />
+    <div id="starship-info">
+      <p id="starship-name">Falcon</p>
+      <StatsTable />
+    </div>
   </div>
 </template>
 
@@ -15,5 +18,14 @@ import StatsTable from '../components/StatsTable.vue';
 #starship-data {
   display: flex;
   justify-content: center;
+}
+
+#starship-name {
+  font-size: 1.4em;
+  font-weight: bold;
+  margin-top: 0;
+  text-align: center;
+  text-decoration: underline double 1px;
+  text-underline-offset: 4px;
 }
 </style>
