a2f24c5bee41d0c5f035a6d20525468f928b5cb4
Eric Arnhem <arnhemeric60@gmail.com>
2022-05-08 00:50:37 +0200

    Add method to handle the form data verification

--- src/components/StarshipForm.vue
+++ src/components/StarshipForm.vue
@@ -2,20 +2,60 @@
 import { ref } from 'vue'
 
 const starshipName = ref('');
-const starshipSpeed = ref(1000);
+const starshipSpeed = ref(100);
 const starshipFuel = ref(1000);
+
+function handleSubmit() {
+  let formErrors = [];
+
+  const regexName = new RegExp(/^[^-\s][\p{L}0-9- ]{1,20}$/gu);
+
+  // Trims the name provided
+  let TrimmedStarshipName = starshipName.value.trim();
+
+  // NAME CHECK
+  // If the name does not match the regex or if the name is too long
+  if (!regexName.test(TrimmedStarshipName) || TrimmedStarshipName.length > 20) {
+    // We add an error to the errors array
+    formErrors.push('nameError');
+  }
+
+  // SPEED CHECK
+  // If the speed is not between 100 and 1000. We add an error.
+  if (starshipSpeed.value < 100 || starshipSpeed.value > 1000) {
+    formErrors.push('speedError');
+  }
+
+  // FUEL CHECK
+  // If the fuel is not between 1000 and 5000. We add an error.
+  if (starshipFuel.value < 1000 || starshipFuel.value > 5000) {
+    formErrors.push('fuelError');
+  }
+
+  // ERRORS CHECK
+  // If there is one or multiple errors
+  if (formErrors.length !== 0) {
+    console.log('There are errors in the form.');
+    console.log(formErrors);
+
+  } else {
+    console.log('Data ready to be sent to the database.');
+  }
+
+}
+
 </script>
 
 <template>
 
-  <form autocomplete="off">
+  <form @submit.prevent="handleSubmit" autocomplete="off">
     <label for="starship-name">Name:</label>
-    <input type="text" name="starship-name" id="starship-name" v-model="starshipName" required>
+    <input type="text" maxlength="20" name="starship-name" id="starship-name" v-model="starshipName" required>
     <label for="starship-speed">Speed:</label>
     <input
       type="number"
-      min="1000"
-      max="5000"
+      min="100"
+      max="1000"
       name="starship-speed"
       id="starship-speed"
       v-model="starshipSpeed"
