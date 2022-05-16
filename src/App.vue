b00a560b154cbace9315f06e5374bbb145180ef6
Eric Arnhem <arnhemeric60@gmail.com>
2022-05-16 22:04:22 +0200

    Change CSS structure for more clarity

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,62 @@ main {
   padding-bottom: 30px;
 }
 
+h1 {
+  text-align: center;
+  text-decoration: underline double 1px;
+  text-underline-offset: 9px;
+}
+
+h2 {
+  border-left: 2px solid var(--main-border-color);
+  padding-left: 10px;
+  margin-top: 0;
+}
+
+/* Navbar */
+nav {
+  height: 100%;
+  width: 45px;
+  border-right: 1px solid var(--main-border-color);
+  box-shadow: 1px 0 1px var(--main-border-color);
+  padding: 0 7px;
+}
+
+nav a {
+  display: inline-block;
+}
+
+nav a.router-link-exact-active {
+  filter: drop-shadow(3px 3px 2px #dddddd);
+}
+
+nav a:hover {
+  filter: drop-shadow(3px 3px 2px #dddddd);
+  transition: 0.1s;
+}
+
+#navigation-links {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+#navigation-links li {
+  border-bottom: 1px solid rgba(146, 146, 146, 0.356);
+  padding-top: 10px;
+  padding-bottom: 5px;
+}
+
+#navigation-links li:last-of-type {
+  border-bottom: none;
+}
+
+#navigation-links li img {
+  display: block;
+  height: 45px;
+}
+
+/* Global classes */
 .text-center {
   text-align: center;
 }
@@ -96,59 +152,5 @@ main {
   width: 40vw;
   min-width: 100%;
   max-width: 650px;
-}
-
-h1 {
-  text-align: center;
-  text-decoration: underline double 1px;
-  text-underline-offset: 9px;
-}
-
-h2 {
-  border-left: 2px solid var(--main-border-color);
-  padding-left: 10px;
-  margin-top: 0;
-}
-
-nav {
-  height: 100%;
-  width: 45px;
-  border-right: 1px solid var(--main-border-color);
-  box-shadow: 1px 0 1px var(--main-border-color);
-  padding: 0 7px;
-}
-
-nav a {
-  display: inline-block;
-}
-
-nav a.router-link-exact-active {
-  filter: drop-shadow(3px 3px 2px #dddddd);
-}
-
-nav a:hover {
-  filter: drop-shadow(3px 3px 2px #dddddd);
-  transition: 0.1s;
-}
-
-#navigation-links {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-#navigation-links li {
-  border-bottom: 1px solid rgba(146, 146, 146, 0.356);
-  padding-top: 10px;
-  padding-bottom: 5px;
-}
-
-#navigation-links li:last-of-type {
-  border-bottom: none;
-}
-
-#navigation-links li img {
-  display: block;
-  height: 45px;
 }
 </style>
