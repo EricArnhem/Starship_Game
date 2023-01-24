f9019d203f3583109fb9c4421851f9195d4c3a2a
Eric Arnhem <arnhemeric60@gmail.com>
2023-01-24 17:46:20 +0100

    Update Axios baseURL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: `http://192.168.1.91:3000/api`,
+  baseURL: `http://192.168.1.66:3000/api`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
