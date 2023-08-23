2db27a3b2c23911d9d81b2bb2dd184e72285a7e4
Eric Arnhem <arnhemeric60@gmail.com>
2023-08-23 17:14:32 +0200

    Add box-shadow when hovering on classes legends

--- src/components/ClassesLegend.vue
+++ src/components/ClassesLegend.vue
@@ -107,6 +107,12 @@ function filterStarshipList(classId) {
   margin-right: 0;
 }
 
+.starship-class-legend:hover {
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px 1px;
+  transition: .1s;
+}
+
 .starship-class-color {
   display: inline-block;
   height: 15px;
