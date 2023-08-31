e1eac9b9e941734e253fd5f26bac80fafb9671d8
Eric Arnhem <arnhemeric60@gmail.com>
2023-08-31 05:39:48 +0200

    Add CreateStarshipCard component

---
+++ src/components/cards/CreateStarshipCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <div id="create-starship-card" class="starship-card">
+    <span id="create-starship-card-title">Create a new Starship</span>
+    <img id="plus-svg" src="@/images/plus.svg" title="Create a new Starship" alt="Plus icon">
+   </div>
+</template>
+
+<style>
+#create-starship-card {
+  justify-content: center;
+  min-height: 245px;
+}
+
+#create-starship-card-title {
+  text-align: center;
+  font-size: 1.4em;
+  font-weight: bold;
+}
+
+#plus-svg {
+  margin-top: 20px;
+  height: 85px;
+}
+
+/* Changes for mobile version */
+@media screen and (max-width: 355px) {
+  #create-starship-card {
+    max-width: 240px;
+  }
+}
+</style>
