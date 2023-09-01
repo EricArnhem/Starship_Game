6417bbf17561bb3fa80c17b0009e35e08157da65
Eric Arnhem <arnhemeric60@gmail.com>
2023-09-01 19:26:30 +0200

    Add JSDoc comments to starship classes API method

--- src/api/methods/starship-class.js
+++ src/api/methods/starship-class.js
@@ -1,5 +1,10 @@
 import { axiosClient } from "../apiClient";
 
-export function getStarshipClasses(){
+/**
+ * Retrieves a list of all the starship classes from the API.
+ * 
+ * @returns {Promise} A Promise that resolves to the list of all starship classes.
+ */
+export function getStarshipClasses() {
     return axiosClient.get('/starship-class');
 }
