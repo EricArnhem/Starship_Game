f8ecf9b07954874a7b795cb7a76d63882a5fea0b
Eric Arnhem <arnhemeric60@gmail.com>
2023-09-09 19:54:27 +0200

    Change path to use segments instead of time

--- src/components/PathTimeline.vue
+++ src/components/PathTimeline.vue
@@ -13,6 +13,7 @@ const emit = defineEmits([
 
 const starshipClassesList = inject('starshipClassesList');
 
+const segmentCount = ref(0);
 const locationCount = ref(0);
 
 const pathDistance = 15000;
@@ -22,8 +23,12 @@ let shipTransitionTimeout;
 // -- Lifecycle Hooks --
 
 onMounted(() => {
-  // Sets ship transition duration based on the current ship speed
-  setTransitionDuration(shipTransitionDuration.value);
+
+  console.log(pathSegmentWidth.value);
+  console.log(pathSegmentsCount.value);
+
+  // Put the starship at the start if it was previously moved
+  resetStarshipPosition();
 
 });
 
@@ -33,36 +38,49 @@ onUnmounted(() => {
 
 // -- Methods --
 
-// Changes the ship transition duration
-function setTransitionDuration(duration) {
-
+// Resets the starship's position back to the start without transition
+function resetStarshipPosition() {
   const shipElement = document.getElementById('ship-svg');
-  shipElement.style.transitionDuration = `${duration}s`;
+  shipElement.style.transitionDuration = '0s';
+  shipElement.style.transform = 'translate(0)';
 
+  // Set back the default transition duration
+  setTimeout(() => {
+    shipElement.style.transitionDuration = '1s';
+  }, 100);
 }
 
 // Moves the ship to the next location (dot)
 function nextLocation() {
 
-  let timelineElement = document.getElementById('timeline');
-  // Getting elements sizes from CSS to calculate the distance for the ship to move
-  let locationDotSize = parseInt(getComputedStyle(timelineElement).getPropertyValue("--location-dot-size"));
-  let locationPathWidth = parseInt(getComputedStyle(timelineElement).getPropertyValue("--location-path-width"));
+  // If we try to move while on the last location
+  if (locationCount.value > 3) {
 
-  // -4 to compensate for the margin on .location-path 
-  let shipAnimationWidth = (locationDotSize + locationPathWidth - 4) * locationCount.value;
+    // Resets locationCount
+    locationCount.value = 0;
+
+    // Moves the starship back to the start
+    resetStarshipPosition()
+
+  }
+
+  segmentCount.value = segmentCount.value + 1;
+
+  // Checks the segment count value to see if we arrived at a location
+  if (segmentCount.value > pathSegmentsCount.value) {
+    // If we arrived at a location, increases the location count
+    locationCount.value = locationCount.value + 1
+    // Resets the segment count
+    segmentCount.value = 0;
+  }
 
   const shipElement = document.getElementById('ship-svg');
 
-  // Animation the ship up to the location wanted
-  shipElement.style.transform = `translateX(${shipAnimationWidth}px)`;
+  // Calculates width based on which segment we are on
+  let width = pathSegmentWidth.value * segmentCount.value;
 
-  // Pause the engines when we arrived at the location
-  shipTransitionTimeout = setTimeout(() => {
-
-    emit('pauseEngines');
-
-  }, shipTransitionDuration.value * 1000);
+  // Animates the starship to the next segment
+  shipElement.style.transform = `translateX(${width}px)`;
 
 }
 
@@ -72,39 +90,48 @@ const starshipSpeed = computed(() => {
   return starshipClassesList.value.find(element => element.id === props.starshipInfo.starshipClassId).speed;
 });
 
-// Rounded duration used for the ship transition
-const shipTransitionDuration = computed(() => {
+// Calculates the distance between two locations (dots)
+const distanceBetweenLocations = computed(() => {
 
-  // Path distance / Ship speed
-  let duration = pathDistance / starshipSpeed.value;
-  let roundedDuration = Math.round(duration);
-  return roundedDuration;
+  // Selecting the #timeline element
+  let timelineElement = document.getElementById('timeline');
 
-});
+  if (timelineElement) {
 
-// -- Watchers --
+    // Getting elements sizes from CSS to calculate the distance for the ship to move
+    let locationDotSize = parseInt(getComputedStyle(timelineElement).getPropertyValue("--location-dot-size"));
+    let locationPathWidth = parseInt(getComputedStyle(timelineElement).getPropertyValue("--location-path-width"));
 
-// Resets locationCount and moves ship back to the start 
-watch(locationCount, (count) => {
+    // -4 to compensate for the margin on .location-path 
+    let distance = (locationDotSize + locationPathWidth - 4);
 
-  // If we try to move while on last location
-  if (count > 3) {
-    // Resets locationCount
-    locationCount.value = 0;
+    return distance;
 
-    // Moves the ship back to the start without transition
-    setTransitionDuration(0);
-    const shipElement = document.getElementById('ship-svg');
-    shipElement.style.transform = 'translateX(0px)';
-
-  } else {
-    setTimeout(() => {
-      // Add back the default transition duration after a timeout otherwise the changed duration gets reverted too quickly
-      setTransitionDuration(shipTransitionDuration.value);
-    }, 100);
   }
 
+  return 0;
+
 });
+
+// Calcutates the number of segments (steps) between one dot to another
+const pathSegmentsCount = computed(() => {
+
+  // Ratio beetween Path distance & Starship speed
+  let distanceSpeedRatio = pathDistance / starshipSpeed.value;
+  let segmentCount = Math.round(distanceSpeedRatio);
+  return segmentCount;
+
+});
+
+// Calculates the width of one segment
+const pathSegmentWidth = computed(() => {
+
+  return distanceBetweenLocations.value / pathSegmentsCount.value;
+
+});
+
+
+// -- Watchers --
 
 // When the parent component tells us to start the animation (variable set to true)
 watch(() => props.animateShip, () => {
@@ -142,7 +169,7 @@ watch(() => props.animateShip, () => {
     </div>
   </div>
 
-  <button @click="locationCount++; nextLocation()" >Move</button>
+  <button @click="nextLocation()" >Move</button>
 
 </template>
 
@@ -150,7 +177,7 @@ watch(() => props.animateShip, () => {
 #timeline {
   --location-dot-size: 30px;
   --location-path-width: 70px;
-  --location-dot-color: #2d2d8d;
+  --location-dot-color: linear-gradient(333deg, #8339c5 0%, #3851d1 70%);
   --location-dot-center-color: #b8b8b8;
   --location-path-color: #474747;
 }
@@ -189,7 +216,7 @@ watch(() => props.animateShip, () => {
 .location-dot {
   height: var(--location-dot-size);
   width: var(--location-dot-size);
-  background-color: var(--location-dot-color);
+  background-image: var(--location-dot-color);
   box-shadow: inset 0px 0px 0px 4px var(--location-dot-center-color);
   border-radius: 50px;
   z-index: 2;
