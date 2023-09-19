0f36a5cb3dd598b12e8040da30a4daaf247130ab
Eric Arnhem <arnhemeric60@gmail.com>
2023-09-19 19:24:28 +0200

    Update encounters choices style

--- src/components/EncountersLog.vue
+++ src/components/EncountersLog.vue
@@ -5,15 +5,13 @@
 
   <div id="starship-encounters">
       <h3>Encounters log</h3>
-      <div id="starship-encounters-choices">
-        <p id="starship-encounters-message">You have encountered a foreign ship.</p>
-        <ul id="starship-encounters-choices-list">
-          <li><button class="button">Option A</button></li>
-          <li><button class="button">Option B</button></li>
-          <li><button class="button">Option C</button></li>
-        </ul>
-      </div>
-    </div>
+      <p id="starship-encounters-message">Waiting for an encounter.</p>
+      <ul id="starship-encounters-choices-list">
+        <li><button class="button">Option A</button></li>
+        <li><button class="button">Option B</button></li>
+        <li><button class="button">Option C</button></li>
+      </ul>
+  </div>
     
 </template>
 
@@ -27,20 +25,15 @@
   text-align: center;
 }
 
-#starship-encounters-choices {
-  width: fit-content;
-  margin: auto;
-}
-
 #starship-encounters-choices-list {
   list-style: none;
   padding: 0;
+  text-align: center;
 }
 
 #starship-encounters-choices-list button {
-  width: 100%;
-
-  margin: 0;
+  width: 80%;
+  margin-top: 0;
   margin-bottom: 10px;
 }
 </style>
